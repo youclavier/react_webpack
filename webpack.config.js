3893bc1245800553818d9dd4f05cd9163e5f0a2c
youclavier <youclavier@gmail.com>
2016-04-14 15:18:32 +0800

    add_react_eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const stylelint = require('stylelint');
 
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event;
@@ -9,24 +10,58 @@ const PATHS = {
     build:  path.join(__dirname, 'build')
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
 
     entry:  {
         app:    PATHS.app
     },
+
+    resolve:    {
+        extensions: ['', '.js', '.jsx']
+    },
+
     output: {
         path: PATHS.build,
         filename:   'bundle.js'
     },
 
     module: {
+        preLoaders: [
+            {
+              test: /\.css$/,
+              loaders:  ['postcss'],
+                include:    PATHS.app
+            },
+            {
+                test:   /\.jsx?$/,
+                loaders:    ['eslint'],
+                include:    PATHS.app
+            }
+        ],
+
+
         loaders:    [
             {
                 test:   /\.css$/,
                 loaders:    ['style', 'css'],
                 include:    PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loaders:    ['babel?cacheDirectory'],
+                include:    PATHS.app
             }
         ]
+    },
+    
+    postcss:    function () {
+        return [stylelint({
+            rules:  {
+                'color-hex-case':   'lower'
+            }
+        })];
     }
 };
 
